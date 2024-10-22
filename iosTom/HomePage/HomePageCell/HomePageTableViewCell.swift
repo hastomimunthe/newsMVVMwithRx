5522e4beeaf61bd4f5d3eb40ba3b5304e5336d57
Hastomi Riduan Munthe <hastomi.riduan10@gmail.com>
2024-10-22 18:40:50 +0700

    feat : search bar, news endpoint, new two pages

---
+++ iosTom/HomePage/HomePageCell/HomePageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageTableViewCell.swift
+//  iosTom
+//
+//  Created by Hastomi Riduan Munthe on 22/10/24.
+//
+
+import UIKit
+
+class HomePageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iosTom/News/NewsCell/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  iosTom
+//
+//  Created by Hastomi Riduan Munthe on 22/10/24.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
